9b25b6d2480bf3f6b5e8d07e9a6eecfaccc1b803
Daniel Górka <daniel.gorka24@gmail.com>
2023-02-25 11:09:33 +0100

    Update scheduler.ts

--- src/scheduler/scheduler.ts
+++ src/scheduler/scheduler.ts
@@ -1,17 +1,28 @@
+import { logger } from "../app";
 import { runATM } from "../atm/monitor";
 import { runPTM } from "../ptm/monitor";
 
-var interval: NodeJS.Timer;
+var interval: NodeJS.Timer | undefined;
 
 // 10 minutes
 const INTERVAL = 1000 * 60 * 10;
 
 export function startMainScheduler() {
+    if (interval !== undefined) {
+        return;
+    }
+
+    logger.info('Main scheduler started');
     interval = setInterval(run, INTERVAL);
     run();
 }
 
 export function stopMainScheduler() {
+    if (interval === undefined) {
+        return;
+    }
+
+    logger.info('Main scheduler stopped');
     clearInterval(interval);
 }
 
