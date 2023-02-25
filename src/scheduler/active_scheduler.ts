b3a2ab0dad5c8f0f440c2d01b6c6533a4dae4704
Daniel Górka <daniel.gorka24@gmail.com>
2023-02-25 11:09:41 +0100

    Create active_scheduler.ts

---
+++ src/scheduler/active_scheduler.ts
@@ -0,0 +1,30 @@
+import { logger } from "../app";
+
+var interval: NodeJS.Timer | undefined;
+
+// 5 seconds
+const INTERVAL = 1000 * 5;
+
+export function startActiveScheduler() {
+    if (interval !== undefined) {
+        return;
+    }
+    
+    logger.info('Active scheduler started');
+    interval = setInterval(run, INTERVAL);
+    run();
+}
+
+export function stopActiveScheduler() {
+    if (interval === undefined) {
+        return;
+    }
+
+    logger.info('Active scheduler stopped');
+    clearInterval(interval);
+}
+
+
+function run() {
+    logger.debug('Active scheduler run');
+}
