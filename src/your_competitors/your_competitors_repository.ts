d75a94dd8edfbc42e887800c656fe2db476a9494
Daniel Górka <daniel.gorka24@gmail.com>
2023-02-25 15:59:20 +0100

    Fix refreshing your competitors

--- src/your_competitors/your_competitors_repository.ts
+++ src/your_competitors/your_competitors_repository.ts
@@ -3,41 +3,38 @@ import * as fs from 'fs/promises';
 import { YourCompetitor } from "./your_competitor";
 
 export const YOUR_COMPETITORS_FILE = 'tmp/your_competitors.json';
-export const YOUR_COMPETITORS_COLLECTION = 'your_compititors';
+export const YOUR_COMPETITORS_COLLECTION = 'your_competitors';
 
 export class YourCompetitorsRepository {
     constructor(private readonly db: FirebaseFirestore.Firestore) { }
 
     public async refreshYourCompetitors(tournamentIds: string[]): Promise<void> {
-        if (tournamentIds.length === 0) {
-            logger.debug('No tournaments to refresh your competitors');
-            return;
-        }
-
-        const snapshot = await this.collection()
-            .where('tournament_id', 'in', tournamentIds)
-            .get();
-
         var list: YourCompetitor[] = [];
 
-        snapshot.forEach((doc) => {
-            const data = doc.data();
-            const comps = data.your_competitors as any[];
-            comps.forEach((comp) => {
-                list.push({
-                    ...comp,
-                    user_id: data.user_id,
-                    tournament_id: data.tournament_id,
+        if (tournamentIds.length > 0) {
+            const snapshot = await this.collection()
+                .where('tournament_id', 'in', tournamentIds)
+                .get();
+
+            snapshot.forEach((doc) => {
+                const data = doc.data();
+                const comps = data.your_competitors as any[];
+                comps.forEach((comp) => {
+                    list.push({
+                        ...comp,
+                        user_id: data.user_id,
+                        tournament_id: data.tournament_id,
+                    });
                 });
             });
-        });
-
+        } else {
+            logger.debug('No tournaments for your competitors');
+        }
 
         const yourCompetitors = {
             your_competitors: list,
             timestamp: new Date().toISOString(),
         };
-
 
         await fs.writeFile(YOUR_COMPETITORS_FILE, JSON.stringify(yourCompetitors, null, 2));
         logger.debug('Your competitors cache refreshed');
