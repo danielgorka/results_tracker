222b15934aa7a4618455952824711414d450ba6c
Daniel Górka <daniel.gorka24@gmail.com>
2023-03-22 22:25:57 +0100

    Add detailed logging when analyzing url

--- src/tournaments/analyze.ts
+++ src/tournaments/analyze.ts
@@ -12,18 +12,29 @@ export async function analyzeUrl(url: string): Promise<boolean> {
 
     try {
         // Get content of the index.html file
-        const response = await fetch(fullUrl);
+        const response = await fetch(fullUrl, {
+            cache: 'no-cache',
+        });
+        if (response.status !== 200) {
+            logger.debug(`Failed to analyze URL ${fullUrl} - status ${response.status}`);
+            return false;
+        }
+
         const html = await response.text();
 
         // Check if this is Shiai page (regex)
         if (SHIAI_REGEX.test(html)) {
-            return true; //todo change to true
+            return true;
         } else {
             logger.debug(`Failed to analyze URL ${fullUrl} - not a Shiai page`);
             return false;
         }
     } catch (e) {
-        logger.debug(`Failed to analyze URL ${fullUrl} - ${e}`);
+        if (e instanceof TypeError) {
+            logger.debug(`Failed to analyze URL ${fullUrl} - ${e.name} ${e.message} ${e.stack}`);
+        } else {
+            logger.debug(`Failed to analyze URL ${fullUrl} - ${e}`);
+        }
         return false;
     }
 }
