55af09a8b381dfb781da2e246c5064e3f438524a
Daniel Górka <daniel.gorka24@gmail.com>
2023-03-25 10:22:35 +0100

    Update ATM to run all requests asynchronously and repeat 4 times

--- src/atm/monitor.ts
+++ src/atm/monitor.ts
@@ -1,6 +1,7 @@
 import { logger, notificationsRepository, tournamentsRepository } from "../app";
 import { AdminNotification } from "../notifications/admin_notification";
 import { analyzeUrl } from "../tournaments/analyze";
+import { Tournament } from "../tournaments/tournament";
 
 
 /// Starts the Available Tournaments Monitor
@@ -47,17 +48,35 @@ export async function runATM(force: boolean = false): Promise<void> {
         return true;
     });
 
-    for (const tournament of tournamentsToAnalyze) {
-        const available = await analyzeUrl(tournament.html_results!.url);
 
-        if (!available) {
-            notificationsRepository.sendAdminNotification(AdminNotification.createTournamentNotAvailable(tournament));
-        }
+    await Promise.all(tournamentsToAnalyze.map(tournament => analyzeTournament(tournament)));
+}
+
+async function analyzeTournament(tournament: Tournament): Promise<void> {
+    // 0 - no proxy request
+    // 1 - proxy request after 5 seconds
+    // 2 - no proxy request after 5 minutes
+    // 3 - proxy request after 5 seconds
+    var available = false;
+    for (var repeatCount = 0; repeatCount < 4; repeatCount++) {
+        available = await analyzeUrl(tournament.html_results!.url, repeatCount % 2 === 1);
 
         if (available) {
             logger.info(`Tournament ${tournament.id} is available.`);
-        } else {
-            logger.info(`Tournament ${tournament.id} is not available. Should be marked as outdated.`);
+            break;
         }
+
+        if (repeatCount % 2 === 0) {
+            logger.info(`Tournament ${tournament.id} is not available. Repeating with proxy in 5 seconds.`);
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        } else if (repeatCount < 4) {
+            logger.info(`Tournament ${tournament.id} is not available (proxy). Repeating in 5 minutes.`);
+            await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
+        }
+    }
+
+    if (!available) {
+        logger.info(`Tournament ${tournament.id} is not available. Should be marked as outdated.`);
+        notificationsRepository.sendAdminNotification(AdminNotification.createTournamentNotAvailable(tournament));
     }
 }
