a7a71875685e222a8cbff1201dd5d3364980f4a4
Daniel Górka <daniel.gorka24@gmail.com>
2023-04-16 14:52:41 +0200

    Add forcing proxy for whitelisted URLs

--- src/core/requests.ts
+++ src/core/requests.ts
@@ -16,11 +16,18 @@ type ProxyRule = 'disabled' | 'retry' | 'force';
  * @returns 
  */
 export async function get(url: string, proxyRule: ProxyRule): Promise<AxiosResponse> {
+    var useProxy = proxyRule === 'force';
+
+    if (proxyRule === 'retry') {
+        // Force proxy if the URL is whitelisted (includes whitelisted string)
+        const proxyUrls = (process.env.CONFIG == null ? require('../../config.json').use_proxy_urls : JSON.parse(process.env.CONFIG!).use_proxy_urls) as string[];
+        useProxy = proxyUrls.some(proxyUrl => url.includes(proxyUrl));
+    }
+
     try {
         var finalUrl: string;
         var headers = {};
-        if (proxyRule === 'force') {
-            // Proxy request
+        if (useProxy) {
             finalUrl = process.env.PROXY_URL!;
             headers = {
                 'Proxy-Auth': process.env.PROXY_AUTH!,
